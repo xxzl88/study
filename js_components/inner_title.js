235c48ce62de8082723c2a8cb3b63d67d6cacca0
xxzl88 <zhanghy152@qq.com>
2026-03-04 17:55:20 +0800

    更新 inner_title.js

--- js_components/inner_title.js
+++ js_components/inner_title.js
@@ -2,6 +2,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const articleTitle = document.querySelector(".article-title");
     if (articleTitle) {
-        document.title = articleTitle.textContent.trim() + " - 我的分享小站";
+        document.title = articleTitle.textContent.trim() + " - 我的分享小站 | 微信公众号xxzl88";
     }
 });
